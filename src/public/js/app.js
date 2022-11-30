ec40e2e4fb6203fc10fc4df7ce56cf56186ef98c
younoLee <es2646526@gmail.com>
2022-11-30 09:26:04 +0900

    noom_websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,14 @@
-// alert("hi!");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () =>{
+    console.log("connect to Server")
+});
+socket.addEventListener("message", (message)=>{
+    console.log(("Just got this:", message,"from the server"))
+});
+socket.addEventListener("close", () =>{
+    console.log("Disconnect from the Server")
+});
+setTimeout(() => {
+    socket.send("hello from browser")
+},5000);
--- src/server.js
+++ src/server.js
@@ -15,10 +15,23 @@ const server = http.createServer(app);
 // Node.js에 기본으로 내장된 Http 패키지를 사용해서 express로 만든 서버 애플리케이션 제공
 const wss = new WebSocket.Server({ server });
 // 웹 소켓 서버를 생성하면서 여기에 HTTP서버를 전당
-function handleConnection(socket){
+/*function handleConnection(socket){
     console.log(socket)
 }
-wss.on("connection", handleConnection);
-server.listen(3000, handleListen);
+//wss의 on메소드에서 전달한 socket를 받기위해  매개변수(socket)를 정의함.
+//socket-> 사용자와 서버  사이의 연결 또는 그에대한 정보를 의미
+//consloe.log(socket)-> socket의 내용 확인
+wss.on("connection", handleConnection);*/
+//웹소켓 서버 wss의 on 메소드-> "connection" 이벤트 발생시 -> 공백함수 handleConnection 호출
+//on 메소드는 롤백함수에 spcket를 전달
 
+wss.on("connection",(socket) => {
+    console.log("connected to Browser")
+    socket.on("close", ()=> console.log("Disconnected from the Server"))
+    socket.on("message",(message)=>{
+        console.log(`${message}`)
+    })
+    socket.send("hello!!")// socket의 send 메소드로 메세지 전당, 서버-> 사용자 ,사용자의 프런트엔드에서 소켓 관련 설정X -> 브라우저에서 볼수 없음
+});
 // console.log("hello")
+server.listen(3000, handleListen);
