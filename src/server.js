253938e1162d344be1d32ee939b619150257e8ec
younoLee <es2646526@gmail.com>
2022-12-16 12:50:12 +0900

    noom v0.12

--- src/server.js
+++ src/server.js
@@ -14,19 +14,23 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer)
 
 wsServer.on("connection", (socket) =>{
+    socket["nickname"] = "Anon";
     socket.on("enter_room", (roomName, done) => {
         done();
         socket.join(roomName);
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
-
-    socket.on("disconnecting",() =>{
-        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+    socket.on("disconnecting", () =>{
+        socket.rooms.forEach((room) => 
+            socket.to(room).emit("bye",socket.nickname)
+        );
     });
+    
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
-    })
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 const handleListen = () => console.log("listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
