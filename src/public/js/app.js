ee776b9eb41587fe95129b17d6f82482f9088d57
younoLee <es2646526@gmail.com>
2022-12-21 13:23:12 +0900

    noom v0.14

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,79 +1,15 @@
 const socket = io();
-
-const welcome = document.getElementById("welcome")
-const form = welcome.querySelector("form")
-const room = document.getElementById("room")
-
-room.hidden = true;
-
-let roomName;
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    const value = input.value;
-    socket.emit("nickname", value,);
-    input.value = "";
-}
-function showRoom(){
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName}`;
-    const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgForm.addEventListener("submit",handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event){
-    event.preventDefault();
-    const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, showRoom);
-    roomName = input.value;
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (userNickname, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${userNickname} arrived!`)
-});
-
-socket.on("bye", (userNickname, newCount) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${userNickname} left ㅠㅠ`);
-});
-socket.on("new_message", (msg) => {
-    addMessage(msg);
-});
-socket.on("room_change",(rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-    if(rooms.length === 0){
-        return;
+const myface= document.getElementById("myface");
+let mystream;
+async function getMedia() {
+    try {
+        mystream= await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            vidio: true,
+        });
+        myface.srcObject= mystream;
+    } catch(e) {
+        console.log(e);
     }
-    rooms.forEach((room) => {
-        const li =document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-})
+}
+getMedia();
