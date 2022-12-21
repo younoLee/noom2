d373aad586bef94513ef3f88c37685c293e08f58
younoLee <es2646526@gmail.com>
2022-12-21 13:23:33 +0900

    noom v0.14

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const socket = io();
 const myface= document.getElementById("myface");
+const muteBin = document.getElementById("mute");
+const cameraBin = document.getElementById("camera");
+
 let mystream;
+let muted = false;
+let cameraOff = false;
+
 async function getMedia() {
     try {
         mystream= await navigator.mediaDevices.getUserMedia({
@@ -13,3 +19,26 @@ async function getMedia() {
     }
 }
 getMedia();
+
+function handleMuteClick(){
+    if(!muted){
+        muteBin.innerText = "Unmute"
+        muted = true;
+    }else{
+        muteBin.innerText = "Mute"
+        muted = false;
+    }
+}
+
+function handleCameraClick(){
+    if(!cameraOff){
+        cameraBin.innerText = "Turn Camera On";
+        cameraOff = true;
+    }else{
+        cameraBin.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+}
+
+muteBin.addEventListener("click", handleMuteClick);
+cameraBin.addEventListener("click", handleCameraClick)
