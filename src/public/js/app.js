7e6505cd41f7750adc8d857b1841a9ee442356a5
younoLee <es2646526@gmail.com>
2022-12-22 11:49:03 +0900

    noom v0.15

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,36 @@
 const socket = io();
-const myface= document.getElementById("myface");
+const myFace= document.getElementById("myFace");
 const muteBin = document.getElementById("mute");
 const cameraBin = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
-let mystream;
+let myStream;
 let muted = false;
 let cameraOff = false;
 
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput")
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            cameraSelect.appendChild(option);
+        });
+    }catch (e){
+        console.log(e);
+    }
+}
+
 async function getMedia() {
     try {
-        mystream= await navigator.mediaDevices.getUserMedia({
+        myStream= await navigator.mediaDevices.getUserMedia({
             audio: true,
-            vidio: true,
+            video: true,
         });
-        myface.srcObject= mystream;
+        myFace.srcObject = myStream;
+        await getCameras();
     } catch(e) {
         console.log(e);
     }
@@ -21,6 +38,8 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick(){
+    myStream.getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if(!muted){
         muteBin.innerText = "Unmute"
         muted = true;
@@ -31,6 +50,8 @@ function handleMuteClick(){
 }
 
 function handleCameraClick(){
+    myStream.getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if(!cameraOff){
         cameraBin.innerText = "Turn Camera On";
         cameraOff = true;
