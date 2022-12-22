d08c6ae8c3d878c63dcd6e0fe1f8a0ed0fc88806
younoLee <es2646526@gmail.com>
2022-12-22 12:53:33 +0900

    noom v0.17

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,15 @@ const myFace= document.getElementById("myFace");
 const muteBin = document.getElementById("mute");
 const cameraBin = document.getElementById("camera");
 const cameraSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
     try{
@@ -42,7 +47,7 @@ async function getMedia(deviceId) {
         console.log(e);
     }
 }
-getMedia();
+
 
 function handleMuteClick(){
     myStream.getAudioTracks()
@@ -72,6 +77,29 @@ async function handleCameraChange(){
     await getMedia(cameraSelect.value);
 }
 
+call.hidden = true;
+
+function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    getMedia();
+}
+
+function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input")
+    socket.emit("join_room",input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
 muteBin.addEventListener("click", handleMuteClick);
-cameraBin.addEventListener("click", handleCameraClick)
-cameraSelect.addEventListener("input", handleCameraChange)
+cameraBin.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", () => {
+    console.log("someone joined!")
+})
