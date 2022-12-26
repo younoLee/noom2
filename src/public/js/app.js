d9cc12c485c0350a2b993abaca3b4b2e759ae000
younoLee <es2646526@gmail.com>
2022-12-27 08:25:32 +0900

    noom v0.20

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -82,7 +82,20 @@ function handleCameraClick(){
 // RTC Code
 
 function makeConnection() {
-    mypeerConnection = new RTCPeerConnection();
+    mypeerConnection = new RTCPeerConnection({
+        iceServers:[
+            {
+                urls:[
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ]
+            }
+        ]
+
+    });
     mypeerConnection.addEventListener("icecandidate",handleIce);
     mypeerConnection.addEventListener("addstream",handleAddStream);
     myStream.getTracks()
